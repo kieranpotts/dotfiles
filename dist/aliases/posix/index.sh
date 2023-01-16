30ed26b6607aee8d48b9630907e45434ef6a2a24
kieranpotts <hello@kieranpotts.com>
2023-01-16 20:43:32 +0000

    feature: add initial shell configurations

---
+++ dist/aliases/posix/index.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# -----------------------------------------------------------------------------
+# Load all POSIX-compliant shell aliases
+# -----------------------------------------------------------------------------
+
+source ~/dotfiles/aliases/posix/cd.sh
+source ~/dotfiles/aliases/posix/git.sh
+source ~/dotfiles/aliases/posix/grep.sh
+source ~/dotfiles/aliases/posix/ls.sh
+source ~/dotfiles/aliases/posix/mkdir.sh
+source ~/dotfiles/aliases/posix/sudo.sh
