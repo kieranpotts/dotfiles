5a50195988135f7666dda25d8e20665e3baba28c
kieranpotts <hello@kieranpotts.com>
2023-01-23 14:14:01 +0000

    feature: backup dotfiles before overwriting

--- bin/update.sh
+++ bin/update.sh
@@ -18,28 +18,44 @@ windows() { [[ -n "$WINDIR" ]]; }
 if windows; then
   # This is required for the `ln` command to generate proper symlinks in
   # MSysGit / Git for Windows. Without this configuration the `ln` command in
-  # this runtime environment will create copies of files, rather than symlinks.
+  # this runtime environment will create copies of the dotfiles rather than
+  # symlinks to them.
   export MSYS=winsymlinks:nativestrict
 fi
 
 # Copy the contents of the `dist` directory into `~/dotfiles`, replacing
 # the files if they already exist in the destination. Note, `cp` will not
-# copy hidden dot files with this command, which is why files like `bashrc`
-# are not dot-prefixed in the `dist` directory.
-cp -Rf dist/* ~/dotfiles
+# copy hidden dot-prefixed files with this command, which is why files like
+# `bashrc` are not dot-prefixed in the `dist` directory.
+cp --recursive --force dist/* ~/dotfiles
+
+# Create backups of `~/.local.**` files, if they exist, before overwriting them.
+# These files are not expected to be references (symlinks) but in case they are
+# we make sure we backup the referenced file, not the symlink.
+cp --dereference ~/.local.gitconfig ~/.backup.gitconfig
+cp --dereference ~/.local.gitignore ~/.backup.gitignore
 
 # Copy the `.local.gitconfig` and `.local.gitignore` templates directly into the
 # user's home directory, unless they already exist there.
-cp -n ~/dotfiles/local.gitconfig ~/.local.gitconfig
-cp -n ~/dotfiles/local.gitignore ~/.local.gitignore
+cp --no-clobber ~/dotfiles/local.gitconfig ~/.local.gitconfig
+cp --no-clobber ~/dotfiles/local.gitignore ~/.local.gitignore
 
-# Create symlinks to the `.gitconfig` and various UNIX profile files. Existing
-# files will not be overwritten. Error output is silenced because it is okay
-# for the linking operation to fail if the files already exist.
-ln -s ~/dotfiles/bash_profile ~/.bash_profile 2> /dev/null
-ln -s ~/dotfiles/bashrc ~/.bashrc 2> /dev/null
-ln -s ~/dotfiles/gitconfig ~/.gitconfig 2> /dev/null
-ln -s ~/dotfiles/profile ~/.profile 2> /dev/null
+# Create backups of home-directory files that will be overwritten. This is
+# especially important on the first install, to make sure the user does not lose
+# existing dotfile configurations. Symbolic links are followed to ensure the
+# actual files are backed up, not just the symlinks to them.
+cp --dereference ~/.bash_profile ~/.backup.bash_profile
+cp --dereference ~/.bashrc ~/.backup.bashrc
+cp --dereference ~/.gitconfig ~/.backup.gitconfig
+cp --dereference ~/.profile ~/.backup.profile
+
+# Create symbolic links (not hard links) to `.gitconfig` and various UNIX files.
+# Existing files will be overwritten (`--force`). Potential errors are NOT
+# silenced because privileges may need to be elevated to do this operation.
+ln --symbolic --force ~/dotfiles/bash_profile ~/.bash_profile
+ln --symbolic --force ~/dotfiles/bashrc ~/.bashrc
+ln --symbolic --force ~/dotfiles/gitconfig ~/.gitconfig
+ln --symbolic --force ~/dotfiles/profile ~/.profile
 
 # Print confirmation message.
-echo 'Updated dotfiles'
+echo '== UPDATED DOTFILES =='
