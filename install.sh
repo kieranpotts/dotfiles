b46b75a6eb1166cb3af26328044da6162c90c932
Kieran Potts <hello@kieranpotts.com>
2024-08-16 05:50:42 +0100

    maintenance: update docs

--- install.sh
+++ install.sh
@@ -86,6 +86,9 @@ if [ -z "${CODESPACES}" ]; then
 else
 
   # Minimal installation for GitHub Codespaces.
+  #
+  # https://docs.github.com/en/codespaces/setting-your-user-preferences/personalizing-github-codespaces-for-your-account#dotfiles
+  # https://docs.github.com/en/codespaces/troubleshooting/troubleshooting-personalization-for-codespaces#troubleshooting-dotfiles
 
   mkdir -p ~/.aliases
   mkdir -p ~/.functions
