47dc3f4434700504f44e4c635ce8b16f7ddfebec
Kieran Potts <hello@kieranpotts.com>
2024-08-25 07:53:50 +0100

    feature: more git aliases

--- dist/aliases/git.sh
+++ dist/aliases/git.sh
@@ -7,8 +7,26 @@
 # Shortcut for `git`.
 alias g="git"
 
-# Shortcut for `lazygit`
+# Shortcut for `lazygit`.
 alias lg="lazygit"
 
-# Shortcut for `gitk`
+# Shortcut for `gitk`.
 alias gk="gitk"
+
+# Shortcuts for common Git commands.
+alias gs="git status"
+alias ga="git add"
+alias gaa="git add -A"
+alias gb="git branch"
+alias gba="git branch -a"
+alias gc="git commit"
+alias gca="git commit -a"
+alias gcm="git commit -m"
+alias gcam="git commit -am"
+alias gpsh="git push"
+alias gpll="git pull"
+alias gco="git checkout"
+alias gsw="git switch"
+alias gmaster="git checkout master"
+alias gmain="git checkout main"
+alias grh="git reset --hard HEAD"
