b6caaa801c2253817f349a4e77edef5a4895ff83
Kieran Potts <hello@kieranpotts.com>
2024-08-25 08:00:20 +0100

    feature: add more aliases

---
+++ dist/aliases/find.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# ==============================================================================
+# Aliases for common `find` operations.
+# ==============================================================================
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
---
+++ dist/aliases/go.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# ==============================================================================
+# Golang aliases.
+# ==============================================================================
+
+alias gocover='go test -coverprofile=coverage.out && go tool cover -html=coverage.out'
+alias gotest='go test $(go list ./... | grep -v /vendor/)'
+alias golint='golint $(go list ./... | grep -v /vendor/)'
