23330a45db81857ac8f6f4668a65c56bc7fc5dc7
Kieran Potts <7712355+kieranpotts@users.noreply.github.com>
2026-05-18 09:40:56 +0100

    feature: remove dependency on deprecated fgrep/egrep - BREAKING

--- dist/aliases/grep.sh
+++ dist/aliases/grep.sh
@@ -4,7 +4,8 @@
 # Aliases for `grep` utils.
 # ==============================================================================
 
-# Always use colorized output.
+# Always use colorized output. `fgrep` and `egrep` are deprecated in GNU grep
+# >= 3.8, so we alias them to the modern equivalents (`grep -F` / `grep -E`).
 alias grep="grep --color=auto"
-alias fgrep="fgrep --color=auto"
-alias egrep="egrep --color=auto"
+alias fgrep="grep -F --color=auto"
+alias egrep="grep -E --color=auto"
